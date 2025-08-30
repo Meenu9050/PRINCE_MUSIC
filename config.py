3b4982cf0670140d081c6907ad128bdaf39a88b8
Meenu9050 <mmsharma9050@gmail.com>
2025-08-30 05:38:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git Updates
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Meenu9050/Kill")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Meenu9050/PRINCE_MUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
